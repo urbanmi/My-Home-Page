17a3cfc07f1fa336b50789b671bf462725df28c9
Urban Mihevc <143194964+urbanmi@users.noreply.github.com>
2024-07-31 09:36:50 +0200

    Second attempt at deployment

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    some_text = "Hello world!"
+    current_year = datetime.now().year
+    cities = ["Boston", "Washington", "New York"]
+    return render_template("index.html", some_text=some_text, current_year=datetime.now().year)
 
 @app.route("/aboutMe")
 def aboutMe():
