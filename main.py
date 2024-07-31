9f59432fe34ca678514b54ad30e05daa7804c9bc
Urban Mihevc <143194964+urbanmi@users.noreply.github.com>
2024-07-31 12:04:09 +0200

    Jinja added

--- main.py
+++ main.py
@@ -7,16 +7,20 @@ app = Flask(__name__)
 def index():
     some_text = "Hello world!"
     current_year = datetime.now().year
-    cities = ["Boston", "Washington", "New York"]
-    return render_template("index.html", some_text=some_text, current_year=datetime.now().year)
+    cities = ["Boston", "Washington", "New York", "Berlin"]
+    return render_template("index.html", some_text=some_text, current_year=datetime.now().year, cities=cities)
 
 @app.route("/aboutMe")
 def aboutMe():
-    return render_template("aboutMe.html")
+    with open("static/aboutMihevc.txt", "r") as aboutFile:
+        aboutList = aboutFile.read().splitlines()
+    return render_template("aboutMe.html", aboutList=aboutList)
 
 @app.route("/portfolio")
 def portfolio():
-    return render_template("portfolio.html")
+    with open("static/portfolio.txt", "r") as portfolioFile:
+        portfolioList = portfolioFile.read().splitlines()
+        return render_template("portfolio.html", portfolioList=portfolioList)
 
 if __name__ == "__main__":
     app.run(use_reloader=False)
