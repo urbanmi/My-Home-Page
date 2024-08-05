1d385d19d70321fc0a12d533bf6463d66e40c3f0
Urban Mihevc <143194964+urbanmi@users.noreply.github.com>
2024-08-05 11:10:06 +0200

    Get/post and cookies

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response
 from datetime import datetime
 
 app = Flask(__name__)
@@ -10,11 +10,22 @@ def index():
     cities = ["Boston", "Washington", "New York", "Berlin"]
     return render_template("index.html", some_text=some_text, current_year=datetime.now().year, cities=cities)
 
-@app.route("/aboutMe")
+@app.route("/aboutMe", methods=["GET", "POST"])
 def aboutMe():
     with open("static/aboutMihevc.txt", "r") as aboutFile:
         aboutList = aboutFile.read().splitlines()
-    return render_template("aboutMe.html", aboutList=aboutList)
+
+    if request.method == "GET":
+        name = request.cookies.get("user-name")
+        return render_template("aboutMe.html", aboutList=aboutList, name=name)
+    if request.method == "POST":
+        contact_name = request.form.get("contact-name")
+        message = request.form.get("contact-message")
+        response = make_response(render_template("success.html"))
+        response.set_cookie("user-name", contact_name)
+        print("contact_name")
+        print("message")
+        return response
 
 @app.route("/portfolio")
 def portfolio():
