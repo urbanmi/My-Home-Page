352e36117e6607b5b71f589ab1b7965ef6ad7c65
Urban Mihevc <143194964+urbanmi@users.noreply.github.com>
2024-08-06 18:26:26 +0200

    SQL database

--- main.py
+++ main.py
@@ -1,37 +1,38 @@
-from flask import Flask, render_template, request, make_response
+from flask import Flask, render_template, request, make_response, redirect
 from datetime import datetime
+
+from sqla_wrapper import SQLAlchemy
+
 
 app = Flask(__name__)
 
+db = SQLAlchemy("sqlite:///db.sqlite")
+
+db_url = os.getenv(os.getenv("DATABASE_URL","sqlite:///db.sqlite").replace("postgres://", "postgresql://", 1)
+db = SQLAlchemy(db_url)
+
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String, unique=False)
+    text = db.Column(db.String, unique=False)
+
+db.create_all()
+
 @app.route("/")
 def index():
-    some_text = "Hello world!"
-    current_year = datetime.now().year
-    cities = ["Boston", "Washington", "New York", "Berlin"]
-    return render_template("index.html", some_text=some_text, current_year=datetime.now().year, cities=cities)
+    messages = db.query(Message).all()
+    return render_template("index.html", messages=messages)
 
-@app.route("/aboutMe", methods=["GET", "POST"])
-def aboutMe():
-    with open("static/aboutMihevc.txt", "r") as aboutFile:
-        aboutList = aboutFile.read().splitlines()
+@app.route("/add-message", methods=["POST"])
+def add_message():
+    username = request.form.get("chat-name")
+    text = request.form.get("chat-message")
+    message = Message(author=username, text=text)
+    message.save()
 
-    if request.method == "GET":
-        name = request.cookies.get("user-name")
-        return render_template("aboutMe.html", aboutList=aboutList, name=name)
-    if request.method == "POST":
-        contact_name = request.form.get("contact-name")
-        message = request.form.get("contact-message")
-        response = make_response(render_template("success.html"))
-        response.set_cookie("user-name", contact_name)
-        print("contact_name")
-        print("message")
-        return response
-
-@app.route("/portfolio")
-def portfolio():
-    with open("static/portfolio.txt", "r") as portfolioFile:
-        portfolioList = portfolioFile.read().splitlines()
-        return render_template("portfolio.html", portfolioList=portfolioList)
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(use_reloader=False)
